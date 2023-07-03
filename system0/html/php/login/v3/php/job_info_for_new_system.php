58660bbe4857f544c26705c480892c7d76f28efe
Janis Steiner <89935073+jakani24@users.noreply.github.com>
2023-07-03 16:26:42 +0200

    Update job_info_for_new_system.php

--- system0/html/php/login/v3/php/job_info_for_new_system.php
+++ system0/html/php/login/v3/php/job_info_for_new_system.php
@@ -50,7 +50,7 @@ $id=$_SESSION["id"];
 						echo '<div style="overflow-x: auto;">';
 						while($cnt!=0)
 						{
-							echo("<table class="table"><thead><tr><th>Keyword</th><th>value</th></tr><t/head></tbody>");
+							echo("<table class='table'><thead><tr><th>Keyword</th><th>value</th></tr><t/head></tbody>");
 							$sql="select id,printer_url,apikey from printer where used_by_userid=$id";
 							$stmt = mysqli_prepare($link, $sql);					
 							mysqli_stmt_execute($stmt);
