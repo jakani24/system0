d7d2a5fee39f76e5b88e31d165035e2e10188ca3
Jamagama <88665828+Jamagama@users.noreply.github.com>
2023-07-03 13:03:23 +0200

    Update job_info_for_new_system.php

--- system0/html/php/login/v3/php/job_info_for_new_system.php
+++ system0/html/php/login/v3/php/job_info_for_new_system.php
@@ -22,70 +22,73 @@ $id=$_SESSION["id"];
 	include "/var/www/html/system0/html/php/login/v3/components.php";
 ?>
 <?php echo(" <body style='background-color:$color'> ");?>
-<div id="content"></div>
 
 <head>
   <title>Your jobs</title>
   
 </head>
 <body>
-<center>
-	<h1>Your running Jobs</h1>
-	<br>
-	<button type="button" class="btn btn-light" href="job_info_for_new_system.php">Reload</button>
-	<br>
-	<br>
-	<?php
-		$cnt=0;
-		$url="";
-		$apikey="";
-		$sql="select count(*) from printer where used_by_userid=$id";
-		$stmt = mysqli_prepare($link, $sql);					
-		mysqli_stmt_execute($stmt);
-		mysqli_stmt_store_result($stmt);
-		mysqli_stmt_bind_result($stmt, $cnt);
-		mysqli_stmt_fetch($stmt);	
-		//echo($cnt);
-		echo '<div style="overflow-x: auto;">';
-		while($cnt!=0)
-		{
-			echo("<table class="table"><thead><tr><th>Keyword</th><th>value</th></tr><t/head></tbody>");
-			$sql="select id,printer_url,apikey from printer where used_by_userid=$id";
-			$stmt = mysqli_prepare($link, $sql);					
-			mysqli_stmt_execute($stmt);
-			mysqli_stmt_store_result($stmt);
-			mysqli_stmt_bind_result($stmt, $id,$url,$apikey);
-			mysqli_stmt_fetch($stmt);
-			//echo("curl $url/api/job?apikey=$apikey > /var/www/html/system0/html/user_files/$username/json.json");
-			exec("curl --max-time 10 $url/api/job?apikey=$apikey > /var/www/html/system0/html/user_files/$username/json.json");
-			$fg=file_get_contents("/var/www/html/system0/html/user_files/$username/json.json");
-			$json=json_decode($fg,true);
-			//var_dump($json);
-			//echo($fg);
-			
-			$progress=(int) $json['progress']['completion'];
-			if($progress<0)
-				$progress=-$progress;
-			$file=$json['job']['file']['name'];
-			//echo($file);
-			echo("<tr><td>average print time</td><td>".$json["job"]["averagePrintTime"]." Seconds</td></tr>");
-			echo("<tr><td>estimated print time</td><td>".round($json["job"]["estimatedPrintTime"],0)." Seconds</td></tr>");
-			echo("<tr><td>filament</td><td>".$json["job"]["filament"]."</td></tr>");
-			echo("<tr><td>file</td><td>".$json["job"]["file"]["name"]."</td></tr>");
-			echo("<tr><td>file size</td><td>".$json["job"]["file"]["size"]." Bytes</td></tr>");
-			echo("<tr><td>progress</td><td>".round($json["progress"]["completion"],2)."%</td></tr>");
-			echo("<tr><td>print time</td><td>".$json["progress"]["printTime"]." Seconds</td></tr>");
-			echo("<tr><td>print time left</td><td>".$json["progress"]["printTimeLeft"]." Seconds</td></tr>");
-
-			$cnt--;
-			echo("</tbody></table>");
-			echo("<iframe height='135' width='240' src='$url/webcam/?action=stream'></iframe>");
-			echo("<br><br>");
-		}
-		//echo("free your printer after you've taken out your print!</div>");
-		//echo("<a href='job.php'>back to job control</a>");
-	?>
-</center>
+	<div class="d-flex justify-content-center align-items-center vh-100">
+      <div class="center">
+      	<h1>Your running Jobs</h1>
+				<br>
+				<button type="button" class="btn btn-primary" href="job_info_for_new_system.php">Reload</button>
+				<br>
+				<br>
+				<div class="container">
+					<?php
+						$cnt=0;
+						$url="";
+						$apikey="";
+						$sql="select count(*) from printer where used_by_userid=$id";
+						$stmt = mysqli_prepare($link, $sql);					
+						mysqli_stmt_execute($stmt);
+						mysqli_stmt_store_result($stmt);
+						mysqli_stmt_bind_result($stmt, $cnt);
+						mysqli_stmt_fetch($stmt);	
+						//echo($cnt);
+						echo '<div style="overflow-x: auto;">';
+						while($cnt!=0)
+						{
+							echo("<table class="table"><thead><tr><th>Keyword</th><th>value</th></tr><t/head></tbody>");
+							$sql="select id,printer_url,apikey from printer where used_by_userid=$id";
+							$stmt = mysqli_prepare($link, $sql);					
+							mysqli_stmt_execute($stmt);
+							mysqli_stmt_store_result($stmt);
+							mysqli_stmt_bind_result($stmt, $id,$url,$apikey);
+							mysqli_stmt_fetch($stmt);
+							//echo("curl $url/api/job?apikey=$apikey > /var/www/html/system0/html/user_files/$username/json.json");
+							exec("curl --max-time 10 $url/api/job?apikey=$apikey > /var/www/html/system0/html/user_files/$username/json.json");
+							$fg=file_get_contents("/var/www/html/system0/html/user_files/$username/json.json");
+							$json=json_decode($fg,true);
+							//var_dump($json);
+							//echo($fg);
+							
+							$progress=(int) $json['progress']['completion'];
+							if($progress<0)
+								$progress=-$progress;
+							$file=$json['job']['file']['name'];
+							//echo($file);
+							echo("<tr><td>average print time</td><td>".$json["job"]["averagePrintTime"]." Seconds</td></tr>");
+							echo("<tr><td>estimated print time</td><td>".round($json["job"]["estimatedPrintTime"],0)." Seconds</td></tr>");
+							echo("<tr><td>filament</td><td>".$json["job"]["filament"]."</td></tr>");
+							echo("<tr><td>file</td><td>".$json["job"]["file"]["name"]."</td></tr>");
+							echo("<tr><td>file size</td><td>".$json["job"]["file"]["size"]." Bytes</td></tr>");
+							echo("<tr><td>progress</td><td>".round($json["progress"]["completion"],2)."%</td></tr>");
+							echo("<tr><td>print time</td><td>".$json["progress"]["printTime"]." Seconds</td></tr>");
+							echo("<tr><td>print time left</td><td>".$json["progress"]["printTimeLeft"]." Seconds</td></tr>");
+				
+							$cnt--;
+							echo("</tbody></table>");
+							echo("<iframe height='135' width='240' src='$url/webcam/?action=stream'></iframe>");
+							echo("<br><br>");
+						}
+						//echo("free your printer after you've taken out your print!</div>");
+						//echo("<a href='job.php'>back to job control</a>");
+					?>
+				</div>
+      </div>
+    </div>
 <br><br><br>
 </body>
 <!-- curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p' -->
