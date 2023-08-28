7fb31dd9f6e8339fc9801e4f17b52c1882719265
Jamagama <88665828+Jamagama@users.noreply.github.com>
2023-08-28 16:50:57 +0200

    Update create_user.php

--- system0/html/php/login/v3/php/create_user.php
+++ system0/html/php/login/v3/php/create_user.php
@@ -109,31 +109,42 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Sign Up</title>
-         <link rel="stylesheet" href="/system0/html/php/login/css/style.css">
 </head>
+<?php 
+	$color=$_SESSION["color"]; 
+	include "/var/www/html/system0/html/php/login/v3/components.php";
+?>
 <body>
-    <div class="wrapper">
-    
-  <div class="container">
-    <h3 class="text-center">Create Account</h3>
-    <form action="" method="post">
-      <div class="form-group">
-        <label for="username">New Username:</label>
-        <input type="text" class="form-control" id="username" name="username" required>
-      </div>
-      <div class="form-group">
-        <label for="pwd">New Password:</label>
-        <input type="password" class="form-control" id="pwd" name="password" required>
-      </div>
-      <div class="form-group">
-        <label for="pwd">Confirm New Password:</label>
-        <input type="password" class="form-control" id="pwd" name="confirm_password" required>
-      </div>
-      <button type="submit" name="submit" class="btn btn-default">Create Account</button>
-    </form>
-    <p>By creating an acocunt you accept our <a href="/system0/html/php/login/v3/php/privacy-policy.php">Privacy Policy</a></p>
-    <center><p>Already have an account? <a href="../login.php">Login here</a>.</p></center>
-  </div>
+    <div class="container mt-5">
+	    <div class="row justify-content-center">
+		    <div class="col-md-6">
+		        <h3 class="text-center">Create Account</h3>
+		        <form action="" method="post">
+    		        <div class="mb-3">
+    				    <label for="username" class="form-label">New Username:</label>
+    					<input type="text" class="form-control" id="username" name="username" required>
+    				</div>
+    				<div class="mb-3">
+    					 <label for="pwd" class="form-label">New Password:</label>
+    					 <input type="password" class="form-control" id="pwd" name="password" required>
+    				</div>
+    				<div class="mb-3">
+    					 <label for="confirmPwd" class="form-label">Confirm New Password:</label>
+    					 <input type="password" class="form-control" id="confirmPwd" name="confirm_password" required>
+    				</div>
+    		        <div class="mb-3 form-check">
+    		            <input type="checkbox" class="form-check-input" id="keepmeloggedin" name="keepmeloggedin" value="keepmeloggedin">
+    		            <label class="form-check-label" for="keepmeloggedin">Keep me logged in</label>
+    		        </div>
+    		        <button type="submit" name="submit" class="btn btn-primary">Create Account</button>
+		        </form>
+		        <div class="text-center mt-3">
+    		        <p class="mt-3">By creating an account you accept our <a href="/system0/html/php/login/v3/php/privacy-policy.php">Privacy Policy</a></p>
+    				<p class="text-center mt-3">Already have an account? <a href="../login.php">Login here</a>.</p>
+	            </div>
+		    </div>
+	    </div>
+	</div>
     <?php 
     if(!empty($err)){
         echo '<div class="alert alert-danger">' . $err . '</div>';
