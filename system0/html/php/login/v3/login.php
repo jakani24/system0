76bf27b1315a7f677abdb96ab746570b5c42d5b6
Jamagama <88665828+Jamagama@users.noreply.github.com>
2023-08-28 17:14:42 +0200

    Update login.php

--- system0/html/php/login/v3/login.php
+++ system0/html/php/login/v3/login.php
@@ -168,36 +168,38 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <title>Login</title>
 </head>
 <body>
-	<div class="container mt-5">
-	  <div class="row justify-content-center">
-	    <div class="col-md-6">
-	      <h3 class="text-center">Login</h3>
-	      <form action="" method="post">
-		<div class="mb-3">
-		  <label for="username" class="form-label">Username:</label>
-		  <input type="text" class="form-control" id="username" name="username" required>
+	<div class="d-flex align-items-center justify-content-center vh-100">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-md-6">
+						<h3 class="text-center">Login</h3>
+						<form action="" method="post">
+							<div class="mb-3">
+								<label for="username" class="form-label">Username:</label>
+								<input type="text" class="form-control" id="username" name="username" required>
+							</div>
+							<div class="mb-3">
+								<label for="pwd" class="form-label">Password:</label>
+								<input type="password" class="form-control" id="pwd" name="password" required>
+							</div>
+							<div class="mb-3 form-check">
+								<input type="checkbox" class="form-check-input" id="keepmeloggedin" name="keepmeloggedin" value="keepmeloggedin">
+								<label class="form-check-label" for="keepmeloggedin">Keep me logged in</label>
+							</div>
+							<button type="submit" name="submit" class="btn btn-primary">Login</button>
+						</form>
+						<div class="text-center mt-3">
+							<a href="php/create_user.php">Don't have an account? Create one!</a>
+						</div>
+						<?php 
+							if(!empty($err)){
+							echo '<div class="alert alert-danger">' . $err . '</div>';
+							}        
+						?>
+					</div>
+				</div>
+			</div>
 		</div>
-		<div class="mb-3">
-		  <label for="pwd" class="form-label">Password:</label>
-		  <input type="password" class="form-control" id="pwd" name="password" required>
-		</div>
-		<div class="mb-3 form-check">
-		  <input type="checkbox" class="form-check-input" id="keepmeloggedin" name="keepmeloggedin" value="keepmeloggedin">
-		  <label class="form-check-label" for="keepmeloggedin">Keep me logged in</label>
-		</div>
-		<button type="submit" name="submit" class="btn btn-primary">Login</button>
-	      </form>
-	      <div class="text-center mt-3">
-		<a href="php/create_user.php">Don't have an account? Create one!</a>
-	      </div>
-		<?php 
-		if(!empty($login_err)){
-		    echo '<div class="alert alert-danger">' . $login_err . '</div>';
-		}        
-		?>
-	    </div>
-	  </div>
-	</div>
 
 
         
