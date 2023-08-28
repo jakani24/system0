99447fab214a619db37eed8b3050a55c0cf72ddc
Janis Steiner <89935073+jakani24@users.noreply.github.com>
2023-08-28 15:09:48 +0000

    Update sys0_client.sh

--- scripts/sys0_client.sh
+++ scripts/sys0_client.sh
@@ -2,4 +2,4 @@
 ngrok http 80 &
 sleep 5
 url=$(curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p' )
-curl --silent https://SYSTEM0_WEBSERVER_URL/system0/html/api/update_url?url=$url&id=ENTER_THE_ID_FOR_THIS_MACHINE_HERE&apikey=YOUR_SYSTEM0_APIKEY&octoapikey=OCTOPRINT_ADMIN_APIKEY
+curl --silent https://SYSTEM0_WEBSERVER_URL/system0/html/api/update_url.php?url=$url&id=ENTER_THE_ID_FOR_THIS_MACHINE_HERE&apikey=YOUR_SYSTEM0_APIKEY&octoapikey=OCTOPRINT_ADMIN_APIKEY
