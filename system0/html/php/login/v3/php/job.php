b4bd4051b78939c36d7cf503ac66a05de5657f64
Jamagama <88665828+Jamagama@users.noreply.github.com>
2023-11-13 16:38:37 +0100

    Update job.php

--- system0/html/php/login/v3/php/job.php
+++ system0/html/php/login/v3/php/job.php
@@ -55,8 +55,7 @@ function load_user()
   
 </head>
 <body>
-	
-<div class="container">
+<div class="container mt-5">
   <div class="row justify-content-center">
     <div class="col-md-6">
 			<h1>Your running Jobs</h1>
