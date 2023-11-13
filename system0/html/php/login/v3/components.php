1d3dd695b073fdb93f2cb67e69529283ccfab3f4
Jamagama <88665828+Jamagama@users.noreply.github.com>
2023-11-13 17:11:47 +0100

    Update components.php

--- system0/html/php/login/v3/components.php
+++ system0/html/php/login/v3/components.php
@@ -1,7 +1,7 @@
 <?php
 echo('<meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" >
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/8ab5a73cf2.js" crossorigin="anonymous"></script>
