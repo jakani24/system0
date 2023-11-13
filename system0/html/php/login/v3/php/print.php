e7666ed30bc211e9f99557d61b71f151219f3e66
Janis Steiner <89935073+jakani24@users.noreply.github.com>
2023-11-13 17:02:17 +0000

    addef footer

--- system0/html/php/login/v3/php/print.php
+++ system0/html/php/login/v3/php/print.php
@@ -26,12 +26,18 @@
 			$(document).ready(function(){
 		   	$('#content').load("/system0/html/php/login/v3/html/admin_page.html");
 			});
+			$(document).ready(function(){
+   		$('#footer').load("/system0/html/php/login/v3/html/footer.html");
+		});
 		}
 		function load_user()
 		{
 			$(document).ready(function(){
 		   	$('#content').load("/system0/html/php/login/v3/html/user_page.html");
 			});
+			$(document).ready(function(){
+   		$('#footer').load("/system0/html/php/login/v3/html/footer.html");
+		});
 		}
 	</script>
 	<?php
