5a929cba4f3989e6f19765fa6f2f19118f960740
Janis Steiner <89935073+jakani24@users.noreply.github.com>
2023-11-13 16:41:54 +0000

    fixed a bug in job display

--- system0/html/php/login/v3/php/job.php
+++ system0/html/php/login/v3/php/job.php
@@ -75,7 +75,7 @@ function load_user()
 					}
 					else
 					{
-						$sql="update printer set free=1,printing=0,cancel=0 ,used_by_userid=0 where id=1";
+						$sql="update printer set free=1,printing=0,cancel=0 ,used_by_userid=0 where id=$printer_id";
 						$stmt = mysqli_prepare($link, $sql);					
 						mysqli_stmt_execute($stmt);
 						sys0_log("User ".$_SESSION["username"]." freed printer ".$_GET["free"]."",$_SESSION["username"],"JOB::PRINTERCTRL::FREE");//notes,username,type
@@ -146,14 +146,15 @@ function load_user()
 				mysqli_stmt_fetch($stmt);	
 				//echo($cnt);
 				echo("<div class='container'><div class='row'><div class='col'><div class='overflow-auto'><table class='table'><thead><tr><th scope='col'>Drucker</th><th scope='col'>Datei</th><th scope='col'>Fertigstellung</th><th scope='col'>Abbrechen</th><th scope='col'>Informationen</th></tr></thead><tbody>");
+				$last_id=0;
 				while($cnt!=0)
 				{
-					$sql="select id,printer_url,apikey,cancel from printer where used_by_userid=$id";
+					$sql="select id,printer_url,apikey,cancel from printer where used_by_userid=$id and id>$last_id order by id";
 					$cancel=0;
 					$stmt = mysqli_prepare($link, $sql);					
 					mysqli_stmt_execute($stmt);
 					mysqli_stmt_store_result($stmt);
-					mysqli_stmt_bind_result($stmt, $id,$url,$apikey,$cancel);
+					mysqli_stmt_bind_result($stmt, $printer_id,$url,$apikey,$cancel);
 					mysqli_stmt_fetch($stmt);
 					//echo("curl $url/api/job?apikey=$apikey > /var/www/html/system0/html/user_files/$username/json.json");
 					exec("curl --max-time 10 $url/api/job?apikey=$apikey > /var/www/html/system0/html/user_files/$username/json.json");
@@ -161,17 +162,17 @@ function load_user()
 					$json=json_decode($fg,true);
 					//var_dump($json);
 					//echo($fg);
-					
+					$last_id=$printer_id;
 					$progress=(int) $json['progress']['completion'];
 					if($progress<0)
 						$progress=-$progress;
 					$file=$json['job']['file']['name'];
 					if($progress==100)
-						echo("<tr><td>$id</td><td>$file</td><td>$progress</td><td><form method='POST' action='?free=$id'><input type='submit' value='free'  name='free'> </form></td><td>Job already finished</td><td><form method='POST' action='job_info.php'><input type='submit' value='detailes'> </form></td></tr>");
+						echo("<tr><td>$printer_id</td><td>$file</td><td>$progress</td><td><form method='POST' action='?free=$printer_id'><input type='submit' value='free'  name='free'> </form></td><td>Job already finished</td><td><form method='POST' action='job_info.php'><input type='submit' value='detailes'> </form></td></tr>");
 					else if($cancel==1)
-						echo("<tr><td>$id</td><td>$file</td><td>cancelled</td><td><form method='POST' action='?free=$id'><input type='submit' value='free'  name='free'> </form></td><td>Job cancelled</td><td><form method='POST' action='job_info.php'><input type='submit' value='detailes'> </form></td></tr>");
+						echo("<tr><td>$printer_id</td><td>$file</td><td>cancelled</td><td><form method='POST' action='?free=$printer_id'><input type='submit' value='free'  name='free'> </form></td><td>Job cancelled</td><td><form method='POST' action='job_info.php'><input type='submit' value='detailes'> </form></td></tr>");
 					else
-						echo("<tr><td>$id</td><td>$file</td><td>$progress%</td><td>Job still running</td><td><form method='POST' action='?cancel=$id'><input type='submit' value='cancel'  name='cancel'> </form></td><td><form method='POST' action='job_info.php'><input type='submit' value='detailes'> </form></td></tr>");
+						echo("<tr><td>$printer_id</td><td>$file</td><td>$progress%</td><td>Job still running</td><td><form method='POST' action='?cancel=$printer_id'><input type='submit' value='cancel'  name='cancel'> </form></td><td><form method='POST' action='job_info.php'><input type='submit' value='detailes'> </form></td></tr>");
 		 			
 					$cnt--;
 				}
@@ -193,18 +194,20 @@ function load_user()
 				mysqli_stmt_fetch($stmt);	
 				//echo($cnt);
 				echo("<div class='container'><div class='row'><div class='col'><div class='overflow-auto'><table class='table'><thead><tr><th>Datei</th><th>Aus der Warteschlange entfernen</th></tr></thead><tbody>");
+				$last_id=0;
 				while($cnt!=0)
 				{
-					$sql="select id,filepath from queue where from_userid=$userid";
+					$sql="select id,filepath from queue where from_userid=$userid and id>$last_id order by id";
 					$cancel=0;
 					$stmt = mysqli_prepare($link, $sql);	
 					echo mysqli_error($link);				
 					mysqli_stmt_execute($stmt);
 					mysqli_stmt_store_result($stmt);
-					mysqli_stmt_bind_result($stmt, $id,$filepath);
+					mysqli_stmt_bind_result($stmt, $queue_id,$filepath);
 					mysqli_stmt_fetch($stmt);
 					$filepath=basename($filepath);
-					echo("<tr><td>$filepath</td><td><form method='POST' action='?remove=$id'><input type='submit' value='remove'  name='remove'> </form></td></tr>");
+					$last_id=$queue_id;
+					echo("<tr><td>$filepath</td><td><form method='POST' action='?remove=$queue_id'><input type='submit' value='remove'  name='remove'> </form></td></tr>");
 		 			
 					$cnt--;
 				}
