9c0759adbb09ec2b509fb96806794c1daf5e8d3f
Jamagama <88665828+Jamagama@users.noreply.github.com>
2023-12-18 18:02:09 +0100

    Update new_main.php

--- system0/html/php/login/v3/php/new_main.php
+++ system0/html/php/login/v3/php/new_main.php
@@ -208,11 +208,11 @@ $id=$_SESSION["id"];
 								}
 								echo("</tbody></table>");
 								//echo("</div>");
-								echo("<div class='alert alert-dark' role='alert'>Gib den Drucker frei, nachdem der Druckvorgang abgeschlossen wurde. Und du deinen Druck abgeholt hast.</div>");
+								echo("<div class='alert alert-dark' role='alert' style='height: 100px;>Gib den Drucker frei, nachdem der Druckvorgang abgeschlossen wurde. Und du deinen Druck abgeholt hast.</div>");
 							}
 							else
 							{
-								echo("<div class='alert alert-dark' role='alert'>Laufende Vorgänge werden hier aufgelistet.</div>");
+								echo("<div class='alert alert-dark' role='alert' style='height: 100px;'>Laufende Vorgänge werden hier aufgelistet.</div>");
 							}
 						?>	
 						<!-- list queue -->
