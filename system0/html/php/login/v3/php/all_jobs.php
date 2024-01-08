adcfe8efd362e51085da8b3d0028a411c2563ec1
Jamagama <88665828+Jamagama@users.noreply.github.com>
2024-01-08 17:10:49 +0100

    Update all_jobs.php

--- system0/html/php/login/v3/php/all_jobs.php
+++ system0/html/php/login/v3/php/all_jobs.php
@@ -128,7 +128,7 @@ function load_user()
 					mysqli_stmt_bind_result($stmt, $cnt);
 					mysqli_stmt_fetch($stmt);	
 					//echo($cnt);
-					echo("<div class='container'><div class='row'><div class='col'><div class='overflow-auto'><table class='table'><thead><tr><th>Printer</th><th>file</th><th>completion</th><th>free</th><th>cancel print</th>></tr></thead><tbody>");
+					echo("<div class='container'><div class='row'><div class='col'><div class='overflow-auto'><table class='table'><thead><tr><th>Printer</th><th>file</th><th>completion</th><th>free</th><th>cancel print</th></tr></thead><tbody>");
 					$last_id=0;					
 					while($cnt!=0)
 					{
