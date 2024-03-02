d84bc7badb9342a01c46bee7a832cb27f1497055
Janis Steiner <89935073+jakani24@users.noreply.github.com>
2024-03-02 19:26:43 +0000

    Update webcam.php

--- system0/html/php/login/v3/php/webcam.php
+++ system0/html/php/login/v3/php/webcam.php
@@ -9,7 +9,7 @@
 	include "queue.php";
 	// Check if the user is logged in, if not then redirect him to login page
 	if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true ){
-	    header("location: login.php");
+	    die("cannot parse session token. Please reload page");
 	    exit;
 	}
 	$username=htmlspecialchars($_SESSION["username"]);
