dbf4408b2b281060ebcad3a56741d22c1b873138
Janis Steiner <89935073+jakani24@users.noreply.github.com>
2024-03-08 22:55:50 +0100

    Add files via upload

---
+++ system0/html/php/login/v3/php/config.php
@@ -0,0 +1,17 @@
+<?php
+define('DB_SERVER', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
+$api="";
+$SENDGRID_API_KEY="";
+$sendgrid_email="";
+$chat_id="";
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
