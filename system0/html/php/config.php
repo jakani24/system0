7d82f962563c16568484bfdd817f6edf4c42a819
Janis Steiner <89935073+jakani24@users.noreply.github.com>
2024-03-18 15:40:31 +0000

    Update config.php

--- system0/html/php/config.php
+++ system0/html/php/config.php
@@ -3,10 +3,10 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'fisch');
 define('DB_PASSWORD', 'tinte');
 define('DB_NAME', 'system0');
-$api="bot6975511033:AAGGswiwKYwCVbehpGE3hz_tLc9xuSAoBVg";
-$SENDGRID_API_KEY="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";
-$sendgrid_email="3dprint@ksw-informatik.ch";
-$chat_id="6587711215";
+$api="";
+$SENDGRID_API_KEY="";
+$sendgrid_email="";
+$chat_id="";
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
