f0f41c68d6ec16718eca0d609a334bc3957f816d
Janis Steiner <89935073+jakani24@users.noreply.github.com>
2024-04-17 17:04:57 +0000

    Update webcam.php

--- system0/html/php/login/v3/php/webcam.php
+++ system0/html/php/login/v3/php/webcam.php
@@ -3,6 +3,7 @@
 	<?php
 		$username=htmlspecialchars($_GET["username"]);
 		$printer_url=$_GET["url"];
+		$rotation=$_GET["rotation"];
 	?>
 	<head>
 	  <title>Webcam</title>
@@ -13,7 +14,7 @@
 			$path = "/var/www/html/system0/html/user_files/$username/$printer_url.jpeg";
 			unlink($path);			
 			exec("wget \"http://$printer_url/webcam/?action=snapshot\" -O $path");
-			echo("<img style='transform: rotate(180deg);' loading='lazy' width='100%' src='/system0/html/user_files/$username/$printer_url.jpeg'>");
+			echo("<img style='transform: rotate(".$rotation."deg);' loading='lazy' width='100%' src='/system0/html/user_files/$username/$printer_url.jpeg'>");
 		?>
 		<script>
 			setInterval(function() {
