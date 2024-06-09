48055f3c3019e405781be43e1beb04d3bc72ffd7
Janis Steiner <89935073+jakani24@users.noreply.github.com>
2024-06-09 19:43:26 +0000

    Update verify_account.php

--- system0/html/php/login/v3/php/verify_account.php
+++ system0/html/php/login/v3/php/verify_account.php
@@ -1,6 +1,34 @@
 <?php
 	echo("<head>");
 	include "/var/www/html/system0/html/php/login/v3/components.php";
+	?>
+	<script>
+		function redirectToAnotherWebsite(newDomain) {
+		    // Get the current URL
+		    const currentUrl = window.location.href;
+
+		    // Create a URL object from the current URL
+		    const url = new URL(currentUrl);
+
+		    // Get the search (query) parameters from the current URL
+		    const searchParams = url.search;
+
+		    // Extract the current domain
+		    const currentDomain = url.origin;
+
+		    // Check if the current domain is different from the new domain
+		    if (currentDomain !== newDomain) {
+			// Construct the new URL with the new domain and the preserved query parameters
+			const newUrl = newDomain + url.pathname + searchParams;
+
+			// Redirect to the new URL
+			window.location.href = newUrl;
+		    }
+		}
+		redirectToAnotherWebsite('https://app.ksw3d.ch');
+	</script>
+	
+	<?php
 	echo("</head>");
 	echo("<br>");
 	session_start();
